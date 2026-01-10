67ca0ea4b1b3fc121aef58a8697208b2b1416939
dgarcez- <dgarcez-@c2r9s5.42lisboa.com>
2026-01-10 16:12:10 +0000

    enemy no bug with asteroids and seems to be working collision

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
+@onready var timer: Timer = $Timer
+#@onready var bullet = preload("res://scenes/Bullet.tscn")
 
-const SPEED = 300.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
-
 
 func _physics_process(delta: float) -> void:
 	var direction := 0
@@ -13,3 +14,14 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	timer.start()
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	timer.stop()
+
+func _on_timer_timeout() -> void:
+	print("shoot")
+	#var bullet_spawn = bullet.instantiate()
+	#get_tree().current_scene.add_child(bullet_spawn)
